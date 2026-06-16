6184b80ecb7848dbf044bcc836ff0e7107ad8c5d
olabamipetaiwo <teeola48@gmail.com>
2026-06-16 19:25:16 +0100

    set up

--- probe.c
+++ probe.c
@@ -40,9 +40,5 @@ int main(void) {
     return 0;
 }
 
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/olabamipetaiwo/csh.git
-git push -u origin main
+
+//   git clone https://github.com/olabamipetaiwo/csh.git
